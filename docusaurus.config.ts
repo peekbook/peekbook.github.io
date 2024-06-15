cf88b4c0e22b6e63203c4bc62f59f39ff9676ca5
salilvnair <salilvnairgithub@gmail.com>
2024-06-15 13:47:44 -0500

    added website config in docusaurus

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -3,22 +3,25 @@ import type {Config} from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
 const config: Config = {
-  title: 'My Site',
+  title: 'Peekbook',
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://peekbook.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
-
-  onBrokenLinks: 'throw',
+  organizationName: 'peekbook', // Usually your GitHub org/user name.
+  projectName: 'peekbook.github.io', // Usually your repo name.
+  
+  deploymentBranch: 'deployment',
+  trailingSlash: false,
+  
+  onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
